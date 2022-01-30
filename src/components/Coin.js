f1f91aa04ceb87d2c91cec4a5acde0b62f501b5c
tlimakun <t.limakun@gmail.com>
2022-01-30 12:17:09 +0700

    Create coin component

---
+++ src/components/Coin.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Image } from "react-native-elements";
+
+// colors
+import colors from "../resources/color-info";
+
+const Coin = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../resources/coin.png")}
+        style={styles.coinImage}
+      />
+      <Text style={styles.coinText}>999</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  coinImage: {
+    width: 25,
+    height: 25,
+  },
+  coinText: {
+    fontFamily: "MarkerFelt",
+    fontSize: 20,
+    color: colors.coin,
+    paddingLeft: 5,
+  },
+});
+
+export default Coin;
