e116a41225de5b198b565c4fc207060ef61d8596
tlimakun <t.limakun@gmail.com>
2022-01-30 11:30:27 +0700

    Create health component

---
+++ src/components/Health.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
+
+// colors
+import colors from "../resources/colorsInfo";
+
+// this constant is dummy constant
+// TODO: Remove this constant when there is max health and health left in redux
+const health = {
+  maxHealth: 3,
+  healthLeft: 2,
+};
+
+// component that show player health left
+const Health = () => {
+  // render number of hearts, depending on player max health and health left
+  const renderHealth = () => {
+    return Array.from(Array(health.maxHealth)).map((item, i) => {
+      return (
+        <Icon
+          key={i}
+          name={i < health.healthLeft ? "heart" : "heart-outlined"}
+          type="entypo"
+          size={25}
+          color={colors.heart}
+        />
+      );
+    });
+  };
+
+  return <View style={styles.container}>{renderHealth()}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: "blue",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
+
+export default Health;
