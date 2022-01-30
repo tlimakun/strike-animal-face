a27f614c3e38d05689d0d2c11d6c9f164e3f8f42
tlimakun <t.limakun@gmail.com>
2022-01-30 12:18:33 +0700

    Create play screen layout

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -1,15 +1,143 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  Dimensions,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Icon } from "react-native-elements";
+
+// components
+import Coin from "../components/Coin";
+import Health from "../components/Health";
+
+// colors
+import colors from "../resources/color-info";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const PlayScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>Play Screen</Text>
+    <SafeAreaView style={mainStyles.container}>
+      <View style={headerStyles.container}>
+        <View style={headerStyles.leftContainer}>
+          <Text style={headerStyles.timerText}>30</Text>
+        </View>
+        <View style={headerStyles.middleContainer}>
+          <Text style={headerStyles.levelText}>NORMAL</Text>
+        </View>
+        <View style={headerStyles.rightContainer}>
+          <TouchableHighlight
+            activeOpacity={0.8}
+            underlayColor={colors.underlayColor}
+            style={headerStyles.button}
+            onPress={() => console.log("Pause a game")}
+          >
+            <Icon name="controller-paus" type="entypo" size={25} />
+          </TouchableHighlight>
+        </View>
+      </View>
+      <View style={bodyStyles.container}>
+        <View style={bodyStyles.boardContainer}></View>
+        <View style={bodyStyles.uiContainer}>
+          <View style={bodyStyles.leftUIContainer}>
+            <Coin />
+          </View>
+          <View style={bodyStyles.middleUIContainer}></View>
+          <View style={bodyStyles.rightUIContainer}>
+            <Health />
+          </View>
+        </View>
+      </View>
+      <View style={footerStyles.container}></View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+// styles for main Container
+const mainStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+// styles for header components
+const headerStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    paddingHorizontal: SCREEN_WIDTH * 0.05,
+  },
+  leftContainer: {
+    flex: 2.5,
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  middleContainer: {
+    flex: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  rightContainer: {
+    flex: 2.5,
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  timerText: {
+    fontFamily: "MarkerFelt",
+    fontSize: 30,
+  },
+  levelText: {
+    fontFamily: "VtcAle",
+    fontSize: 25,
+  },
+  button: {
+    width: 36,
+    height: 36,
+    borderWidth: 3,
+    borderRadius: 7,
+    borderColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+// styles for body components
+const bodyStyles = StyleSheet.create({
+  container: {
+    flex: 7,
+    paddingHorizontal: SCREEN_WIDTH * 0.05,
+  },
+  boardContainer: {
+    borderTopWidth: 3,
+    borderBottomWidth: 3,
+    borderColor: "#000000",
+    flex: 9,
+  },
+  uiContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  leftUIContainer: {
+    flex: 3,
+  },
+  middleUIContainer: {
+    flex: 4,
+    justifyContent: "center",
+  },
+  rightUIContainer: {
+    flex: 3,
+    justifyContent: "center",
+  },
+});
+
+// styles for footer components
+const footerStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default PlayScreen;
