170117dceb356906ca6d597b472460d8ef1ec9af
tlimakun <t.limakun@gmail.com>
2022-01-30 12:10:55 +0700

    Add level info

---
+++ src/resources/level-info.js
@@ -0,0 +1,29 @@
+export default {
+  easy: {
+    roundTime: 30,
+    timeBtwAppear: [0.5, 1.25],
+    appearTime: [0.75, 1.75],
+    maxHealth: 3,
+    extraTime: 1,
+    extraTimePercent: 0.3,
+    obtainedCoin: 1,
+  },
+  normal: {
+    roundTime: 30,
+    timeBtwAppear: [0.25, 1],
+    appearTime: [0.5, 1.5],
+    maxHealth: 3,
+    extraTime: 1,
+    extraTimePercent: 0.2,
+    obtainedCoin: 2,
+  },
+  hard: {
+    roundTime: 30,
+    timeBtwAppear: [0.01, 0.75],
+    appearTime: [0.25, 1.25],
+    maxHealth: 3,
+    extraTime: 1,
+    extraTimePercent: 0.2,
+    obtainedCoin: 3,
+  },
+};
